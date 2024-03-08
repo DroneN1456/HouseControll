ecdeffe1883b53b8b2b7a278fb84aea00d816799
DroneN1456 <gugagrilo07@gmail.com>
2024-03-08 12:18:44 -0300

    testes no servidor

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest){
     const token = request.cookies.get('token')
 
+    console.log(token)
     const url = request.nextUrl.clone()
     url.pathname = '/auth/login'
     if(!token){
@@ -18,7 +19,9 @@ export async function middleware(request: NextRequest){
         method: "POST",
         body: JSON.stringify({token: token.value})
     })
+    console.log(res)
     const data = await res.json()
+    console.log(data)
     if(data.statusCode){
         return NextResponse.redirect(url)
     }
